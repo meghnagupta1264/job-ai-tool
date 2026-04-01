84acfee9308d0342d5bc00219f4ceadb87d142c9
meghna gupta <owlscarlet1264@gmail.com>
2026-04-02 01:11:57 +0530

    connect frontend to render backend

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://meghnagupta1264.github.io', 'http://localhost:3000']
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.static(__dirname));
 
